0219dc2ec813bb3124f27136e3fd27410870b5c4
Claus Nielsen <Claus Nielsen@labtop-windows>
2009-02-21 08:54:41 +0100

    New methods: listFilesRecursively and addFilesRecursively

--- src/main/java/dk/clanie/io/FileUtil.java
+++ src/main/java/dk/clanie/io/FileUtil.java
@@ -17,11 +17,14 @@
  */
 package dk.clanie.io;
 
+import static dk.clanie.util.CollectionFactory.newArrayList;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.channels.FileChannel;
 import java.nio.channels.spi.AbstractInterruptibleChannel;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,9 +36,97 @@ import org.slf4j.LoggerFactory;
  */
 public class FileUtil {
 
-	private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(FileUtil.class);
 
 	static final String FAILED_TO_CLOSE_CHANNEL = "Failed to close channel.";
+
+	/**
+	 * Creates a copy of a file.
+	 * 
+	 * Shorthand for <code>copyFile(from, to, false)</code>, <code>false</code>
+	 * meaning don't overwrite existing file.
+	 * 
+	 * @param from
+	 *            - source File
+	 * @param to
+	 *            - destination File
+	 * 
+	 * @throws RuntimeIOException
+	 */
+	public static void copyFile(File from, File to) throws RuntimeIOException {
+		copyFile(from, to, false);
+	}
+
+	/**
+	 * Creates a copy of a file.
+	 * 
+	 * If the destination is a directory the file is copied to that directory
+	 * with the same name as the source file.
+	 * 
+	 * @param from
+	 *            - source File
+	 * @param to
+	 *            - destination File
+	 * @param overwrite
+	 *            - allow overwriting existing file
+	 * 
+	 * @throws RuntimeIOException
+	 */
+	public static void copyFile(File from, File to, boolean overwrite)
+			throws RuntimeIOException {
+		FileChannel inputChannel = null;
+		FileChannel outputChannel = null;
+		try {
+			if (!from.exists())
+				throw new RuntimeIOException.FileNotFound(from);
+			if (to.isDirectory()) {
+				to = new File(to.getPath() + File.separator + from.getName());
+			}
+			if (to.exists()) {
+				if (!overwrite)
+					throw new RuntimeIOException.FileAlreadyExists(to);
+			} else if (!to.createNewFile())
+				throw new RuntimeIOException.FailedToCreate(to);
+
+			inputChannel = new FileInputStream(from).getChannel();
+			outputChannel = new FileOutputStream(to).getChannel();
+			long size = inputChannel.size();
+			long copied = 0L;
+			while (copied < size)
+				copied += outputChannel.transferFrom(inputChannel, copied, size
+						- copied);
+		} catch (java.io.IOException e) {
+			throw new RuntimeIOException(e.getMessage(), e);
+		} finally {
+			closeChannels(inputChannel, outputChannel);
+		}
+	}
+
+	/**
+	 * Closes nio Channels.
+	 * <p>
+	 * Suppresses errors - if an Exception occurs it will be logged, but
+	 * otherwise silently ignored.
+	 * </p>
+	 * <p>
+	 * Nulls in the channels argument are ignored.
+	 * </p>
+	 * 
+	 * @param channels
+	 *            - AbstractInterruptibleChannels to close.
+	 */
+	public static void closeChannels(AbstractInterruptibleChannel... channels) {
+		for (AbstractInterruptibleChannel channel : channels) {
+			if (channel == null)
+				continue;
+			try {
+				channel.close();
+			} catch (Exception e) {
+				logger.error(FAILED_TO_CLOSE_CHANNEL, e);
+			}
+		}
+	}
 
 	/**
 	 * Deletes a file or directory.
@@ -56,87 +147,36 @@ public class FileUtil {
 	}
 
 	/**
-	 * Creates a copy of a file.
+	 * Creates a List of all the files in a directory and it's sub-directories.
 	 * 
-	 * Shorthand for <code>copyFile(from, to, false)</code>, <code>false</code>
-	 * meaning don't overwrite existing file.
-	 * 
-	 * @param from
-	 *            - source File
-	 * @param to
-	 *            - destination File
-	 * 
-	 * @throws RuntimeIOException
+	 * @param dir
+	 *            the directory to scan.
+	 * @return List of all Files in the directory and it's sub-directories.
 	 */
-	public static void copyFile(File from, File to) throws RuntimeIOException {
-		copyFile(from, to, false);
+	public List<File> listFilesRecursively(File dir) {
+		List<File> fileList = newArrayList();
+		addFilesRecursively(dir, fileList);
+		return fileList;
 	}
 
-
 	/**
-	 * Creates a copy of a file.
+	 * Adds all files in a directory and it's sub-directories to the supplied
+	 * List.
 	 * 
-	 * If the destination is a directory the file is copied to that directory
-	 * with the same name as the source file.
-	 * 
-	 * @param from
-	 *            - source File
-	 * @param to
-	 *            - destination File
-	 * @param overwrite
-	 *            - allow overwriting existing file
-	 * 
-	 * @throws RuntimeIOException
+	 * @param dir
+	 *            the directory to scan.
+	 * @param fileList
+	 *            all files in the specified directory and it's sub-directories.
 	 */
-	public static void copyFile(File from, File to, boolean overwrite) throws RuntimeIOException {
-		FileChannel inputChannel = null;
-		FileChannel outputChannel = null;
-		try {
-			if (!from.exists()) throw new RuntimeIOException.FileNotFound(from);
-			if (to.isDirectory()) {
-				to = new File(to.getPath() + File.separator + from.getName());
-			}
-			if (to.exists()) {
-				if (!overwrite) throw new RuntimeIOException.FileAlreadyExists(to);
-			} else if (!to.createNewFile())
-				throw new RuntimeIOException.FailedToCreate(to);
-
-			inputChannel = new FileInputStream(from).getChannel();
-			outputChannel = new FileOutputStream(to).getChannel();
-			long size = inputChannel.size();
-			long copied = 0L;
-			while (copied < size)
-				copied += outputChannel.transferFrom(inputChannel, copied, size - copied);
-		} catch (java.io.IOException e) {
-			throw new RuntimeIOException(e.getMessage(), e);
-		} finally {
-			closeChannels(inputChannel, outputChannel);
-		}
-	}
-
-
-	/**
-	 * Closes nio Channels.
-	 * <p>
-	 * Suppresses errors - if an Exception occurs it will be logged, but
-	 * otherwise silently ignored.
-	 * </p><p>
-	 * Nulls in the channels argument are ignored.
-	 * </p>
-	 * 
-	 * @param channels
-	 *            - AbstractInterruptibleChannels to close.
-	 */
-	public static void closeChannels(AbstractInterruptibleChannel... channels) {
-		for (AbstractInterruptibleChannel channel : channels) {
-			if (channel == null) continue;
-			try {
-				channel.close();
-			} catch (Exception e) {
-				logger.error(FAILED_TO_CLOSE_CHANNEL, e);
+	public void addFilesRecursively(File dir, List<File> fileList) {
+		File[] files = dir.listFiles();
+		for (File file : files) {
+			if (file.isDirectory())
+				addFilesRecursively(file, fileList);
+			else {
+				fileList.add(file);
 			}
 		}
 	}
-
 
 }
