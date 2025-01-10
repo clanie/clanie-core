2e7448d07bfb5745b505487ac26f48465b1826fe
Claus Nielsen <clausn999@gmail.com>
2025-01-10 17:25:31 +0100

    Moved utility functions from clanie-portfolio into this project.

--- src/main/java/dk/clanie/core/Utils.java
+++ src/main/java/dk/clanie/core/Utils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2024, Claus Nielsen, clausn999@gmail.com
+ * Copyright (C) 2024-2025, Claus Nielsen, clausn999@gmail.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/test/java/dk/clanie/core/util/StringUtilsTest.java
+++ src/test/java/dk/clanie/core/util/StringUtilsTest.java
@@ -17,27 +17,17 @@
  */
 package dk.clanie.core.util;
 
-import static dk.clanie.core.Utils.stream;
+import static dk.clanie.core.util.StringUtils.csv;
 import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-class CollectionUtilsTest {
+class StringUtilsTest {
 
 
 	@Test
-	void testStreamIterable() {
-		List<Integer> ints = List.of(1, 2, 3);
-		assertThat(stream(ints).mapToInt(Integer::intValue).sum()).isEqualTo(6);
-	}
-
-
-	@Test
-	void testStreamArray() {
-		Integer[] ints = {1, 2, 3};
-		assertThat(stream(ints).mapToInt(Integer::intValue).sum()).isEqualTo(6);
+	void testCsv_null() {
+		assertThat(csv(null)).isEqualTo("");
 	}
 
 
