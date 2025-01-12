e2608abc9fd0ffc25bfa3e63b8de0eca4b9a3533
Claus Nielsen <clausn999@gmail.com>
2025-01-12 12:54:17 +0100

    Fix resource paths in JsonServiceTest

--- src/test/java/dk/clanie/core/util/JsonServiceTest.java
+++ src/test/java/dk/clanie/core/util/JsonServiceTest.java
@@ -48,7 +48,7 @@ public class JsonServiceTest {
 
 	@Test
 	public void testLoad_class() {
-		KeyAndValue dto = json.load("dk/clanie/core/util/jsonServiceTest/keyValuePair.json", KeyAndValue.class);
+		KeyAndValue dto = json.load("dk/clanie/core/util/JsonServiceTest/keyValuePair.json", KeyAndValue.class);
 		assertThat(dto.getK()).isEqualTo("key");
 		assertThat(dto.getV()).isEqualTo("value");
 	}
@@ -56,7 +56,7 @@ public class JsonServiceTest {
 
 	@Test
 	public void testLoad_array() {
-		KeyAndValue[] dtos = json.load("dk/clanie/core/util/jsonServiceTest/keyValuePairs.json", KeyAndValue[].class);
+		KeyAndValue[] dtos = json.load("dk/clanie/core/util/JsonServiceTest/keyValuePairs.json", KeyAndValue[].class);
 		assertThat(dtos[0].getK()).isEqualTo("key");
 		assertThat(dtos[0].getV()).isEqualTo("value");
 		assertThat(dtos[1].getK()).isEqualTo("key2");
@@ -67,7 +67,7 @@ public class JsonServiceTest {
 
 	@Test
 	public void testLoad_list() {
-		List<KeyAndValue> dtos = json.load("dk/clanie/core/util/jsonServiceTest/keyValuePairs.json", new TypeReference<List<KeyAndValue>>() {});
+		List<KeyAndValue> dtos = json.load("dk/clanie/core/util/JsonServiceTest/keyValuePairs.json", new TypeReference<List<KeyAndValue>>() {});
 		assertThat(dtos.get(0).getK()).isEqualTo("key");
 		assertThat(dtos.get(0).getV()).isEqualTo("value");
 		assertThat(dtos.get(1).getK()).isEqualTo("key2");
@@ -78,7 +78,7 @@ public class JsonServiceTest {
 
 	@Test
 	public void testLoad_genericType() {
-		KeyValuePair<String, String> keyValuePair = json.load("dk/clanie/core/util/jsonServiceTest/keyValuePair.json", new TypeReference<KeyValuePair<String, String>>() {});
+		KeyValuePair<String, String> keyValuePair = json.load("dk/clanie/core/util/JsonServiceTest/keyValuePair.json", new TypeReference<KeyValuePair<String, String>>() {});
 		assertThat(keyValuePair.getK()).isEqualTo("key");
 		assertThat(keyValuePair.getV()).isEqualTo("value");
 	}
