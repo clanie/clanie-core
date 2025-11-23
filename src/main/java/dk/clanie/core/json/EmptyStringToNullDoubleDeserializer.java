873986bd166f053fd9e651cfd2a81f7709ce4985
Claus Nielsen <clausn999@gmail.com>
2025-11-23 02:23:25 +0100

    Added EmptyStringToNullDoubleDeserializer

---
+++ src/main/java/dk/clanie/core/json/EmptyStringToNullDoubleDeserializer.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2025, Claus Nielsen, clausn999@gmail.com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package dk.clanie.core.json;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+/**
+ * Deserializer that converts empty strings to null for Double fields.
+ *
+ * Used for fields which can be either a number or an empty string.
+ */
+public class EmptyStringToNullDoubleDeserializer extends JsonDeserializer<Double> {
+
+	@Override
+	public Double deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+		String value = p.getText();
+		if (value == null || value.trim().isEmpty()) {
+			return null;
+		}
+		return Double.parseDouble(value);
+	}
+
+}
